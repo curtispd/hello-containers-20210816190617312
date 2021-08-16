32981abd5badb2219cdacc500dd8eb31d601a540
Curtis d'Entremont <curtis.dentremont@gmail.com>
2021-08-16 15:20:35 -0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
 
 
   
-const welcomeMessage = "Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service";
-const portMessage = "Application Running on port";
+const welcomeMessage = "zzzWelcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service";
+const portMessage = "zzzApplication Running on port";
 
 const getWelcomeMessage = () => {
     return welcomeMessage;
